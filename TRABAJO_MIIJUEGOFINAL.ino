3bc65c14b01c680f6a35190141b52965fafe0215
LeonelPino <pinoleonel1016@gmail.com>
2025-06-22 20:40:46 -0500

    Update TRABAJO_MIIJUEGOFINAL.ino

--- TRABAJO_MIIJUEGOFINAL.ino
+++ TRABAJO_MIIJUEGOFINAL.ino
@@ -1,1 +1,111 @@
+#include <LiquidCrystal.h>
 
+#define PIN_BUTTON 2
+#define PIN_AUTOPLAY 1
+#define PIN_READWRITE 10
+#define PIN_CONTRAST 12
+
+#define SPRITE_RUN1 1
+#define SPRITE_RUN2 2
+#define SPRITE_JUMP 3
+#define SPRITE_JUMP_UPPER '.'         // Use the '.' character for the head
+#define SPRITE_JUMP_LOWER 4
+#define SPRITE_TERRAIN_EMPTY ' '      // Use the ' ' character
+#define SPRITE_TERRAIN_SOLID 5
+#define SPRITE_TERRAIN_SOLID_RIGHT 6
+#define SPRITE_TERRAIN_SOLID_LEFT 7
+
+#define HERO_HORIZONTAL_POSITION 1    // Posición horizontal del héroe en la pantalla
+
+#define TERRAIN_WIDTH 16
+#define TERRAIN_EMPTY 0
+#define TERRAIN_LOWER_BLOCK 1
+#define TERRAIN_UPPER_BLOCK 2
+
+#define HERO_POSITION_OFF 0          // Heore es invisible
+#define HERO_POSITION_RUN_LOWER_1 1  // El héroe está corriendo en la fila superior (pose 1)
+#define HERO_POSITION_RUN_LOWER_2 2  // El héroe está corriendo en la fila superior (pose 2)
+
+#define HERO_POSITION_JUMP_1 3       // Starting a jump
+#define HERO_POSITION_JUMP_2 4       // Half-way up
+#define HERO_POSITION_JUMP_3 5       // Jump is on upper row
+#define HERO_POSITION_JUMP_4 6       // Jump is on upper row
+#define HERO_POSITION_JUMP_5 7       // Jump is on upper row
+#define HERO_POSITION_JUMP_6 8       // Jump is on upper row
+#define HERO_POSITION_JUMP_7 9       // Half-way down
+#define HERO_POSITION_JUMP_8 10      // About to land
+
+#define HERO_POSITION_RUN_UPPER_1 11 // Hero is running on upper row (pose 1)
+#define HERO_POSITION_RUN_UPPER_2 12 // Hero is running on upper row (pose 2)
+
+LiquidCrystal lcd(11, 9, 6, 5, 4, 3);
+static char terrainUpper[TERRAIN_WIDTH + 1];
+static char terrainLower[TERRAIN_WIDTH + 1];
+static bool buttonPushed = false;
+
+void initializeGraphics(){
+  static byte graphics[] = {
+    // Run position 1
+    B01100,
+    B01100,
+    B00000,
+    B01110,
+    B11100,
+    B01100,
+    B11010,
+    B10011,
+    // Run position 2
+    B01100,
+    B01100,
+    B00000,
+    B01100,
+    B01100,
+    B01100,
+    B01100,
+    B01110,
+    // Jump
+    B01100,
+    B01100,
+    B00000,
+    B11110,
+    B01101,
+    B11111,
+    B10000,
+    B00000,
+    // Jump lower
+    B11110,
+    B01101,
+    B11111,
+    B10000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    // Ground
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    // Ground right
+    B00011,
+    B00011,
+    B00011,
+    B00011,
+    B00011,
+    B00011,
+    B00011,
+    B00011,
+    // Ground left
+    B11000,
+    B11000,
+    B11000,
+    B11000,
+    B11000,
+    B11000,
+    B11000,
+    B11000,
+  };
